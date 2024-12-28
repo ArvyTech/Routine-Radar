d4d9e6929c70ccbc6dc3ca066d9bd751bd23b311
ArvyTech <rikshitvashishtha11@gmail.com>
2024-12-28 20:16:18 +0530

    URL added

--- back-end/routineradar/tracker/urls.py
+++ back-end/routineradar/tracker/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import TechnologyList, ActivityList
+from .views import get_feedback
 
 urlpatterns = [
     path('technologies/', TechnologyList.as_view(), name='technology-list'),
     path('activities/', ActivityList.as_view(), name='activity-list'),
+    path('api/feedback/', get_feedback, name='get_feedback'),
 ]
